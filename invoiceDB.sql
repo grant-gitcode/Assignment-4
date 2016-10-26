c5f921d5a401e7e4104cad9a884e238518e998f9
grant-gitcode <grant.tbird@gmail.com>
2016-10-26 11:33:02 -0500

    Update invoiceDB.sql

--- invoiceDB.sql
+++ invoiceDB.sql
@@ -5,30 +5,34 @@ drop table Person;
 drop table Address;
 drop table Email;
 select * from Person;
-alter table Person drop column ;
+alter table Person drop column emailID;
+alter table Person drop column addressID;
 alter table Person drop foreign key Person_ibfk_1;
 alter table Person drop foreign key Person_ibfk_2;
 show create table Person;
+set foreign_key_checks = 0;
+
 -- show tables;
-
-CREATE TABLE Email (
-emailID int(11) not null auto_increment,
-personID int,
-emailAddess VARCHAR(30),
-primary key (personID),
-foreign key (personID) references Person(personID)
-);
-
 
 CREATE TABLE Address (
 addressID int (11) not null auto_increment,
+street varchar(30),
 city varchar(30),
 state varchar(30),
-street varchar(30),
 zip varchar(30),
 country varchar(30),
 primary key (addressID)
 );
+
+-- Creation of Address sample data -- 
+insert into Address(street, city, state, zip, country) values (
+4
+944c;Lesner, Rocky;321 Backwards Street,Dallas,IA,60789,USA;rockythelesner@boxerbro.com, rockyarockmysocks@socksbro.net
+306a;McCringleberry, Hingle;2906 N. 34th St.,Funky Town,WY,60999,USA;thecringleberry@gmail.com
+55bb;Wise, Yoda;0000 Meditation Way,Portland,OR,75111,USA;YodaWise@gmail.com, YodaWise@hotmail.com, YodaWise@outlook.com
+2342;Thorton, Billy Bob;no street available,no city,no state,no zip,no nation;
+
+
 
 create table Person (
 personID int(11) not null auto_increment,
@@ -38,6 +42,22 @@ lastName varchar(30),
 addressID int,
 primary key (personID),
 foreign key (addressID) references Address(addressID)
+);
+
+-- Creation of Person sample data -- 
+select * from Person;
+delete from Person where personID = 1;
+INSERT INTO Person(personCode,firstName,lastName,addressID) VALUES("944c","Rocky","Lesner",1);
+INSERT INTO Person(personCode,firstName,lastName,addressID) VALUES("306a","Hingle","McCringleberry",2);
+INSERT INTO Person(personCode,firstName,lastName,addressID) VALUES("55bb","Yoda","Wise",3);
+INSERT INTO Person(personCode,firstName,lastName,addressID) VALUES("2342","Billy Bob","Thorton",4);
+
+CREATE TABLE Email (
+emailID int(11) not null auto_increment,
+personID int,
+emailAddess VARCHAR(30),
+primary key (emailID),
+foreign key (personID) references Person(personID)
 );
 
 alter table Email add foreign key (personID) references Person(personID);
@@ -110,7 +130,3 @@ endDate Date,
 primary key (productsID),
 foreign key (invoiceID) references Invoice(invoiceID)
 );
-
--- Now a query to input sample data -- 
-
-INSERT INTO Email VALUES(
